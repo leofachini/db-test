cbe40c3b3221d23d1870052f0a579856ca94aee6
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-07 00:06:56 +0100

    Enabled CORS headers in the route

--- backend/src/api/components/logger/logger.router.js
+++ backend/src/api/components/logger/logger.router.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const LoggerController = require('./logger.controller');
 
-router.post('/log', LoggerController.processLogFile);
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+router.post('/log', cors(corsOptions), LoggerController.processLogFile);
 
 module.exports = router;
